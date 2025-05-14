010038bbafbf7feb61aa7653b12dc8d738554d32
johnmasataka <johnmasataka@outlook.com>
2025-05-14 15:10:29 -0400

    20250514_Updates

--- script.js
+++ script.js
@@ -15,8 +15,8 @@ const languageContent = {
         // conceptDetail: "To maximize the potential of a place, simply providing a 'box' is not enough. We propose 'content' that makes each building have its own story. Centered around the 'SHARE' project, we will develop our business with a range of sizes and programs all over Japan.",
         profile: "PROFILE",
         project: "PROJECTS<br>(Selected from Columbia University)",
-        shareProject: "Archtalk",
-        shareProjectDetail: "The project started with 'SHAREyaraicho' in Yaraicho, a residential area in Kagurazaka. Our motivation is revitalizing the invisible community in Tokyo by making a series of share houses. In our SHARE projects, every share house has very unique common space besides living room, shower room, toilet, laundry and working space.",
+        // shareProject: "Archtalk",
+        // shareProjectDetail: "The project started with 'SHAREyaraicho' in Yaraicho, a residential area in Kagurazaka. Our motivation is revitalizing the invisible community in Tokyo by making a series of share houses. In our SHARE projects, every share house has very unique common space besides living room, shower room, toilet, laundry and working space.",
         // About section
         aboutTitle: "John Masataka Xianfeng Jiang",
         aboutContent: "John is a New York / Tokyo-based architect, AI researcher, and project manager. He has over eight years of professional experience at <a href=\"https://kkaa.co.jp/en/\" target=\"_blank\" style=\"color: inherit;\">Kengo Kuma & Associates (KKAA)</a>, where he has led teams on a wide range of projects, including luxury towers, resorts, private residences, and high-end furniture. His unique career bridges the confluence of management, technology, and design. He also worked for the <a href=\"https://c4sr.columbia.edu/\" target=\"_blank\" style=\"color: inherit;\">Center for Spatial Research (CSR)</a> at Columbia University, and is the member of the <a href=\"https://www.aij.or.jp/aijhome.htm\" target=\"_blank\" style=\"color: inherit;\">Architectural Institute of Japan (AIJ)</a>. John holds master's degree in computation in <a href=\"https://www.arch.columbia.edu/\" target=\"_blank\" style=\"color: inherit;\">Columbia University GSAPP</a> and architecture in <a href=\"https://www.global.hokudai.ac.jp/\" target=\"_blank\" style=\"color: inherit;\">Hokkaido University</a>.",
@@ -39,10 +39,10 @@ const languageContent = {
         profile: "プロフィール",
         project: "プロジェクト<br>(コロンビア大学での作品のみ)",
         // shareProject: "SHAREプロジェクトとは",
-        shareProjectDetail: "神楽坂の住宅地、矢来町にある「SHAREyaraicho」からはじまったプロジェクト。シェアハウスを通して、東京の見えなくなってしまったコミュニティを再び可視化させたい思いから始動。どの家も、プライベートな部屋のほかに、リビングやシャワー、トイレ、ランドラリー、ワーキングスペースなどの様々なコモンスペースを持つのが特徴。",
+        // shareProjectDetail: "神楽坂の住宅地、矢来町にある「SHAREyaraicho」からはじまったプロジェクト。シェアハウスを通して、東京の見えなくなってしまったコミュニティを再び可視化させたい思いから始動。どの家も、プライベートな部屋のほかに、リビングやシャワー、トイレ、ランドラリー、ワーキングスペースなどの様々なコモンスペースを持つのが特徴。",
         // About section
         aboutTitle: "ジョン・賢峰・蔣",
-        aboutContent: "蔣はニューヨークと東京を拠点とする建築家、AI研究者、プロジェクトマネージャー。隈研吾建築都市設計事務所（<a href=\"https://kkaa.co.jp/en/\" target=\"_blank\" style=\"color: inherit;\"></a>）にてマネージャーを務め、高層集合住宅、リゾート、個人住宅、高級家具など、幅広い分野のプロジェクトに携わる。キャリアの中心にあるのは、マネジメント・テクノロジー・デザインの三領域をまたぐ横断的な実践。また、コロンビア大学の<a href=\"https://c4sr.columbia.edu/\" target=\"_blank\" style=\"color: inherit;\">空間研究センター（Center for Spatial Research）</a>に所属し、<a href=\"https://www.aij.or.jp/aijhome.htm\" target=\"_blank\" style=\"color: inherit;\">日本建築学会</a>正会員。蔣は<a href=\"https://www.arch.columbia.edu/\" target=\"_blank\" style=\"color: inherit;\">コロンビア大学</a>でコンピューティション学科卒業、<a href=\"https://www.global.hokudai.ac.jp/\" target=\"_blank\" style=\"color: inherit;\">北海道大学</a>で建築学科修了。",
+        aboutContent: "蔣はニューヨークと東京を拠点とする建築家、AI研究者、プロジェクトマネージャー。<a href=\"https://kkaa.co.jp/en/\" target=\"_blank\" style=\"color: inherit;\">隈研吾建築都市設計事務所</a> (KKAA) にてマネージャーを務め、高層集合住宅、リゾート、個人住宅、高級家具など、幅広い分野のプロジェクトに携わる。キャリアの中心にあるのは、マネジメント・テクノロジー・デザインの三領域をまたぐ横断的な実践。また、コロンビア大学の<a href=\"https://c4sr.columbia.edu/\" target=\"_blank\" style=\"color: inherit;\">空間研究センター（Center for Spatial Research）</a>に所属し、<a href=\"https://www.aij.or.jp/aijhome.htm\" target=\"_blank\" style=\"color: inherit;\">日本建築学会</a>正会員。蔣は<a href=\"https://www.arch.columbia.edu/\" target=\"_blank\" style=\"color: inherit;\">コロンビア大学</a>でコンピューティション学科卒業、<a href=\"https://www.global.hokudai.ac.jp/\" target=\"_blank\" style=\"color: inherit;\">北海道大学</a>で建築学科修了。",
         // Footer
         copyright: "Copyright © - All Rights Reserved."
     },
@@ -63,8 +63,8 @@ const languageContent = {
         profile: "PERFIL",
         project: "PROYECTOS<br>(seleccionados de la Universidad de Columbia)",
         // shareProject: "¿Qué es el Proyecto SHARE?",
-        shareProjectDetail: "El proyecto comenzó con 'SHAREyaraicho' en Yaraicho, un área residencial en Kagurazaka. Nuestra motivación es revitalizar la comunidad invisible en Tokio mediante la creación de una serie de casas compartidas. En nuestros proyectos SHARE, cada casa compartida tiene un espacio común único además de sala de estar, ducha, baño, lavandería y espacio de trabajo.",
-        // About section
+        // shareProjectDetail: "El proyecto comenzó con 'SHAREyaraicho' en Yaraicho, un área residencial en Kagurazaka. Nuestra motivación es revitalizar la comunidad invisible en Tokio mediante la creación de una serie de casas compartidas. En nuestros proyectos SHARE, cada casa compartida tiene un espacio común único además de sala de estar, ducha, baño, lavandería y espacio de trabajo.",
+        // // About section
         aboutTitle: "John Masataka Xianfeng Jiang",
         aboutContent: "John es un arquitecto, investigador de IA y gerente de proyectos con sede en Nueva York/Tokio. Tiene más de ocho años de experiencia profesional en <a href=\"https://kkaa.co.jp/en/\" target=\"_blank\" style=\"color: inherit;\">Kengo Kuma & Associates (KKAA)</a>, donde ha liderado equipos en una amplia gama de proyectos, incluyendo torres de lujo, resorts, residencias privadas y muebles de alta gama. Su carrera única une la gestión, la tecnología y el diseño. También trabajó para el <a href=\"https://c4sr.columbia.edu/\" target=\"_blank\" style=\"color: inherit;\">Center for Spatial Research (CSR)</a> en la Universidad de Columbia y es miembro del <a href=\"https://www.aij.or.jp/aijhome.htm\" target=\"_blank\" style=\"color: inherit;\">Architectural Institute of Japan (AIJ)</a>. John tiene una maestría en computación en <a href=\"https://www.arch.columbia.edu/\" target=\"_blank\" style=\"color: inherit;\">Columbia University</a> y en arquitectura en <a href=\"https://www.global.hokudai.ac.jp/\" target=\"_blank\" style=\"color: inherit;\">Hokkaido University</a>.",
         // Footer
@@ -87,8 +87,8 @@ const languageContent = {
         profile: "프로필",
         project: "프로젝트<br>(컬럼비아 대학교 작품 중)",
         // shareProject: "SHARE 프로젝트란?",
-        shareProjectDetail: "이 프로젝트는 카구라자카의 주거 지역인 야라이초에 있는 'SHAREyaraicho'에서 시작되었습니다. 우리의 동기는 쉐어하우스 시리즈를 만들어 도쿄의 보이지 않는 커뮤니티를 활성화하는 것입니다. SHARE 프로젝트에서는 모든 쉐어하우스가 거실, 샤워실, 화장실, 세탁실 및 작업 공간 외에도 매우 독특한 공용 공간을 가지고 있습니다.",
-        // About section
+        // shareProjectDetail: "이 프로젝트는 카구라자카의 주거 지역인 야라이초에 있는 'SHAREyaraicho'에서 시작되었습니다. 우리의 동기는 쉐어하우스 시리즈를 만들어 도쿄의 보이지 않는 커뮤니티를 활성화하는 것입니다. SHARE 프로젝트에서는 모든 쉐어하우스가 거실, 샤워실, 화장실, 세탁실 및 작업 공간 외에도 매우 독특한 공용 공간을 가지고 있습니다.",
+        // // About section
         aboutTitle: "존 마사타카 시안펑 장",
         aboutContent: "존은 뉴욕/도쿄를 기반으로 하는 건축가, AI 연구원, 프로젝트 매니저입니다. 그는 <a href=\"https://kkaa.co.jp/en/\" target=\"_blank\" style=\"color: inherit;\">건고쿠마 앤드 어소시에이츠(KKAA)</a>에서 8년 이상의 전문 경력을 가지고 있으며, 럭셔리 타워, 리조트, 개인 주택, 하이엔드 가구 등 다양한 프로젝트에서 팀을 이끌었습니다. 그의 독특한 경력은 경영, 기술, 디자인의 융합을 연결합니다. 그는 또한 컬럼비아 대학의 <a href=\"https://c4sr.columbia.edu/\" target=\"_blank\" style=\"color: inherit;\">공간 연구 센터(CSR)</a>에서 일했으며 <a href=\"https://www.aij.or.jp/aijhome.htm\" target=\"_blank\" style=\"color: inherit;\">일본 건축 학회(AIJ)</a>의 회원입니다. 존은 <a href=\"https://www.arch.columbia.edu/\" target=\"_blank\" style=\"color: inherit;\">컬럼비아 대학 GSAPP</a>에서 계산학 석사, <a href=\"https://www.global.hokudai.ac.jp/\" target=\"_blank\" style=\"color: inherit;\">홋카이도 대학</a>에서 건축학 석사를 취득했습니다.",
         // Footer
@@ -111,7 +111,7 @@ const languageContent = {
         profile: "简介",
         project: "项目<br>(仅限哥伦比亚大学期间)",
         // shareProject: "什么是SHARE项目",
-        shareProjectDetail: "该项目始于神乐坂住宅区矢来町的'SHAREyaraicho'。我们的动机是通过创建一系列共享住宅来振兴东京看不见的社区。在我们的SHARE项目中，每个共享住宅除了客厅、淋浴间、厕所、洗衣房和工作空间外，还有非常独特的公共空间。",
+        // shareProjectDetail: "该项目始于神乐坂住宅区矢来町的'SHAREyaraicho'。我们的动机是通过创建一系列共享住宅来振兴东京看不见的社区。在我们的SHARE项目中，每个共享住宅除了客厅、淋浴间、厕所、洗衣房和工作空间外，还有非常独特的公共空间。",
         // About section
         aboutTitle: "蒋贤峰",
         aboutContent: "蒋贤峰，字君安 (John)，是一位常驻纽约和东京的建筑师、AI研究员和项目经理。他在<a href=\"https://kkaa.co.jp/en/\" target=\"_blank\" style=\"color: inherit;\">隈研吾建筑都市设计事务所（KKAA）</a>拥有超过八年的专业经验，曾领导团队完成各种项目，包括豪华塔楼、度假村、私人住宅和高端家具。他的职业生涯专注于管理、技术和设计。他还在<a href=\"https://c4sr.columbia.edu/\" target=\"_blank\" style=\"color: inherit;\">哥伦比亚大学空间研究中心（CSR）</a>工作，并且是<a href=\"https://www.aij.or.jp/aijhome.htm\" target=\"_blank\" style=\"color: inherit;\">日本建筑学会（AIJ）</a>的成员。蒋贤峰在<a href=\"https://www.arch.columbia.edu/\" target=\"_blank\" style=\"color: inherit;\">哥伦比亚大学</a>获得计算学硕士学位，在<a href=\"https://www.global.hokudai.ac.jp/\" target=\"_blank\" style=\"color: inherit;\">北海道大学</a>获得建筑学硕士学位。",
@@ -135,7 +135,7 @@ const languageContent = {
         profile: "簡介",
         project: "作品<br>(哥倫比亞大學)",
         // shareProject: "什麼是SHARE項目",
-        shareProjectDetail: "該項目始於神樂坂住宅區矢來町的'SHAREyaraicho'。我們的動機是通過創建一系列共享住宅來振興東京看不見的社區。在我們的SHARE項目中，每個共享住宅除了客廳、淋浴間、廁所、洗衣房和工作空間外，還有非常獨特的公共空間。",
+        // shareProjectDetail: "該項目始於神樂坂住宅區矢來町的'SHAREyaraicho'。我們的動機是通過創建一系列共享住宅來振興東京看不見的社區。在我們的SHARE項目中，每個共享住宅除了客廳、淋浴間、廁所、洗衣房和工作空間外，還有非常獨特的公共空間。",
         // About section
         aboutTitle: "蔣賢峰",
         aboutContent: "蔣賢峰，字君安（John），是一位長期居住於紐約與東京的建築師、AI 研究員以及專案經理。他在<a href=\"https://kkaa.co.jp/en/\" target=\"_blank\" style=\"color: inherit;\">隈研吾建築都市設計事務所（KKAA）</a>擁有超過八年的專業經驗，曾領導團隊完成多項專案，包括豪華高樓、度假村、私人住宅與高端家具設計。他的職涯專注於管理、技術與設計三大領域。他亦曾於<a href=\"https://c4sr.columbia.edu/\" target=\"_blank\" style=\"color: inherit;\">哥倫比亞大學空間研究中心（CSR）</a>工作，並為<a href=\"https://www.aij.or.jp/aijhome.htm\" target=\"_blank\" style=\"color: inherit;\">日本建築學會（AIJ）</a>的成員。蔣賢峰擁有<a href=\"https://www.arch.columbia.edu/\" target=\"_blank\" style=\"color: inherit;\">哥倫比亞大學</a>計算學碩士學位，以及<a href=\"https://www.global.hokudai.ac.jp/\" target=\"_blank\" style=\"color: inherit;\">北海道大學</a>建築學碩士學位。",
